151480554e7c087524d92197652c0c595d787c48
Braden Simpson <bradensimpsons@gmail.com>
2023-08-29 11:40:53 -0700

    fix: cleanup imports in simple example

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,8 +1,8 @@
 import { Network } from '../../src/resources/graphql'
-import { Defined } from '../../src/sdk'
+import { Defined } from '@definedfi/sdk/dist/sdk'
 
 const sdk = new Defined(process.env.DEFINED_API_KEY || "")
 
 sdk.send<{ getNetworks: Network[] }>(`query GetNetworks { getNetworks { id name } }`, {}).then(res => {
-  console.log("COMPLETE", res)
+  console.log("Networks: ", res.getNetworks)
 })
