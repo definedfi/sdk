25c00f1cfbc615ef4e4b2e7a7dc5bcb8a3a897e7
Braden Simpson <bradensimpsons@gmail.com>
2023-08-29 14:28:21 -0700

    fix: codegen package needs correct package name

--- examples/codegen/codegen.ts
+++ examples/codegen/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "node_modules/@defined/sdk/src/resources/schema.graphql",
+  schema: "node_modules/@definedfi/sdk/src/resources/schema.graphql",
   documents: "src/**/*.ts",
   generates: {
     "src/gql/": {
