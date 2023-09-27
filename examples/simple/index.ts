5cf621b1ee36e41125b790944dac8cf2694ac334
Braden Simpson <bradensimpsons@gmail.com>
2023-09-26 22:09:06 -0700

    chore: cleanup unused imports

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -6,3 +6,8 @@ const sdk = new Defined(process.env.DEFINED_API_KEY || "")
 sdk.send<{ getNetworks: Network[] }>(`query GetNetworks { getNetworks { id name } }`, {}).then(res => {
   console.log("Networks: ", res.getNetworks)
 })
+
+sdk.queries.price({ inputs: [{ address: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", networkId: 56 }]}).then(console.log)
+sdk.queries.pairEvents({ query: { address: "0xd99c7f6c65857ac913a8f880a4cb84032ab2fc5b", networkId: 56 } }).then(console.log)
+sdk.queries.sparklines({ input: { ids: ["0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c:56"] } }).then(console.log)
+sdk.queries.token({ input: { address: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", networkId: 56 } }).then(console.log)
--- src/sdk/Query.ts
+++ src/sdk/Query.ts
@@ -1,13 +1,10 @@
 import { Defined } from "./index";
-import { print } from 'graphql'
-import { OnPriceUpdatedDocument, GetSparklinesDocument, GetSparklinesQueryVariables, GetTokenPriceDocument, GetTokensDocument, GetTokensQuery, QueryGetTokenPricesArgs, SubscriptionOnPriceUpdatedArgs, PairEventsDocument, PairEventsQueryVariables } from "./gql/graphql";
-import { QueryGetTokensInfoArgs } from "../resources/graphql";
-import { Sink } from "graphql-ws";
+import { GetSparklinesDocument, GetSparklinesQueryVariables, GetTokenPriceDocument, GetTokensDocument, GetTokensQuery, QueryGetTokenPricesArgs, SubscriptionOnPriceUpdatedArgs, PairEventsDocument, PairEventsQueryVariables, QueryTokenArgs } from "./gql/graphql";
 
 export class Query {
   constructor(private sdk: Defined) { }
   pairEvents = async (vars: PairEventsQueryVariables) => { return this.sdk.query(PairEventsDocument, vars) }
   sparklines = async (vars: GetSparklinesQueryVariables) => { return this.sdk.query(GetSparklinesDocument, vars) }
-  token = async (vars: QueryGetTokensInfoArgs) => { return this.sdk.query(GetTokensDocument, vars) }
+  token = async (vars: QueryTokenArgs) => { return this.sdk.query(GetTokensDocument, vars) }
   price = async (vars: QueryGetTokenPricesArgs) => { return this.sdk.query(GetTokenPriceDocument, vars) }
 }
